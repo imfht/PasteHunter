75d79ceda58df911787d0e6eefb5015e2114b1b8
jinxufang <jinxufang@tencent.com>
2019-01-08 21:13:28 +0800

    add mongo output

---
+++ outputs/mongo_output.py
@@ -0,0 +1,19 @@
+import logging
+
+import pymongo
+
+from common import parse_config
+
+logger = logging.getLogger('pastehunter')
+
+config = parse_config()
+
+
+class MongoOutput:
+    def __init__(self):
+        self.mongo_cli = pymongo.MongoClient()['pasteHunter']  # FIXME: should read from config file.
+
+    def store_paste(self, paste_data):
+        if not config['outputs']['json_output']['store_raw']:
+            del paste_data['raw_paste']
+        self.mongo_cli['data'].insert_one(paste_data)
